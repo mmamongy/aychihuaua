439b267e2e08c57b2c5885a8535bf1576987d0db
Maryam Mamdouh <mmamdouh@uplandsoftware.com>
2022-07-10 14:40:25 +0200

    updating home and adding contact us

--- src/app/public/public-routing.module.ts
+++ src/app/public/public-routing.module.ts
@@ -1,3 +1,7 @@
+import { BarbucksComponent } from './barbucks/barbucks.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { TeamComponent } from './team/team.component';
+import { ServicesComponent } from './services/services.component';
 import { QuestionsComponent } from './questions/questions.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MainComponent } from './main/main.component';
@@ -7,12 +11,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  {
-    path: 'home',
-    component: HomePageComponent,
-  },
+  { path: 'home', component: HomePageComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'get-started', component: QuestionsComponent}
+  { path: 'services', component: ServicesComponent },
+  { path: 'get-started', component: QuestionsComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'barbucks', component: BarbucksComponent },
+  
 ];
 
 @NgModule({
