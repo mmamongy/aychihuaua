5658943460acf554175da990038747fbb04df043
Maryam Mamdouh <mmamdouh@uplandsoftware.com>
2022-08-12 14:43:12 +0200

    adding scroll and animation to home page and barbuck page

--- src/app/public/barbucks/barbucks.component.ts
+++ src/app/public/barbucks/barbucks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import AOS from 'aos';
 @Component({
   selector: 'app-barbucks',
   templateUrl: './barbucks.component.html',
@@ -10,6 +10,9 @@ export class BarbucksComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init({disable: 'mobile'});//AOS - 2
+    AOS.refresh();//refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
+
   }
 
 }
--- src/app/public/home-page/home-page.component.ts
+++ src/app/public/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
+
 
 @Component({
   selector: 'app-home-page',
@@ -10,6 +12,9 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init({disable: 'mobile'});//AOS - 2
+    AOS.refresh();//refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
+
   }
 
 }
